f11905a88bade20a4fbf611cc0395f2ce184b8bb
varshith016 <108548279+varshith016@users.noreply.github.com>
2022-07-06 22:54:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model = joblib.load('FOOD-INGREDIENTS')
+st.title("FOOD & IT'S INGREDIENTS")
+ip = st.text_input('Enter the food name')
+op = model.predict([ip])
+if st.button('Go'):
+  st.title(op[0])
